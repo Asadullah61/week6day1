ef16e3f0781458a1c692522471acf797536c8ce4
Asadullah61 <asadmari61@gmail.com>
2026-02-16 11:49:34 +0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,25 @@
+
+# Week 6 Backend Project
+
+## Setup Instructions
+
+1. Install Node.js
+2. Install MongoDB locally
+3. Extract this project
+4. Run:
+
+npm install
+npm run dev
+
+Server runs on:
+http://localhost:5000
+
+## API Routes
+
+GET     /
+GET     /products
+POST    /products
+PUT     /products/:id
+DELETE  /products/:id
+
+Use Postman to test APIs.
---
+++ server.js
@@ -0,0 +1,61 @@
+
+// ===============================
+// Week 6 Backend Project
+// Instructor: Sohail Ahmed
+// ===============================
+
+const express = require('express');
+const mongoose = require('mongoose');
+const Product = require('./models/Product');
+
+const app = express();
+const PORT = 5000;
+
+app.use(express.json());
+
+// MongoDB Connection
+mongoose.connect('mongodb://127.0.0.1:27017/week6db')
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.log(err));
+
+// Home Route
+app.get('/', (req, res) => {
+    res.send("Week 6 Backend Server Running");
+});
+
+// CREATE Product
+app.post('/products', async (req, res) => {
+    try {
+        const product = new Product(req.body);
+        await product.save();
+        res.json(product);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+});
+
+// READ All Products
+app.get('/products', async (req, res) => {
+    const products = await Product.find();
+    res.json(products);
+});
+
+// UPDATE Product
+app.put('/products/:id', async (req, res) => {
+    const updated = await Product.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        { new: true }
+    );
+    res.json(updated);
+});
+
+// DELETE Product
+app.delete('/products/:id', async (req, res) => {
+    await Product.findByIdAndDelete(req.params.id);
+    res.json({ message: "Product Deleted" });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
